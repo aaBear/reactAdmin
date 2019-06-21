553b472e2a24293bdda153945d77350f32d189d6
bear <75598853@qq.com>
2019-06-21 21:03:12 +0800

    完成登录页表单校验

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import { Form, Icon, Input, Button } from 'antd';
 
 import logo from './logo.png';
 import './index.less';
 
+// 缓存
 const Item = Form.Item;
 
-class Login extends Component {
 
+class Login extends Component {
     login = (e) => {
         e.preventDefault();
+        // 进行校验
+        this.props.form.validateFields((error, values) => {
+            if (!error) {
+                const { username, password } = values;
+                console.log(`用户名: ${username} 密码: ${password}`)
+            } else {
+                console.log('登录表单校验失败：', error);
+            }
+        })
     }
     
+    // 校验逻辑
+    validator = (rule, value, callback) => {
+        const name = rule.field === 'username' ? '用户名' : '密码';
+        if (!value) {
+            callback(`请输入${name}`);
+        } else if (value.length < 3) {
+            callback(`${name}必须大于2位`);
+        } else if (value.length > 8) {
+            callback(`${name}必须小于9位`);
+        } else if (!/^[a-zA-Z0-9]+$/.test()) {
+            callback(`${name}只能包含字母和数字`);
+        } else {
+            callback();
+        }
+    }
+
     render() {
-        // const { getFieldDecorator } = this.props.form;
+        // 获取表单校验方法
+        const { getFieldDecorator } = this.props.form;
         return (
             <div className="login">
                 <header className="login-header">
@@ -24,20 +51,35 @@ class Login extends Component {
                     <h2>用户登陆</h2>
                     <Form onSubmit={this.login} className="login-form">
                         <Item>
-                            <Input className="login-input" prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="用户名"></Input>
+                            {
+                                getFieldDecorator(
+                                    'username',
+                                    {rules: [{validator: this.validator}]}
+                                )(
+                                    <Input className="login-input" prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="用户名"></Input>
+                                )
+                            } 
+                            
                         </Item>
                         <Item>
-                            <Input className="login-input" prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="密码" type="password"></Input>
+                            {
+                                getFieldDecorator(
+                                    'password',
+                                    {rules: [{validator: this.validator}]}
+                                )(
+                                    <Input className="login-input" prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="密码" type="password"></Input>
+                                )
+                            }
                         </Item>
                         <Item>
                             <Button className="login-btn" type="primary" htmlType="submit">登陆</Button>
                         </Item>
                     </Form>
                 </section>
-                
             </div>
         )
     }
 }
 
-export default Login;
+// 高阶组件增加Form属性，用于表单校验
+export default Form.create()(Login);
