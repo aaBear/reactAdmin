8279e3cdd9b9def70a9926a5cfd1824e056e93d5
bear <75598853@qq.com>
2019-06-24 21:14:56 +0800

    完成主页home菜单页面

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        Home
+      <div style={{textAlign: 'center', height: 360, lineHeight: '360px', fontSize: 30}}>
+        欢迎使用硅谷后台管理系统
       </div>
     )
   }
